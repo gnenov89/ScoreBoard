3cfb89fe71ed686878250d233f3b47d4ed1820a9
gnenov89 <georgi.nenov89@gmail.com>
2018-03-15 17:17:32 -0400

    Stopwatch component ready

---
+++ candy/src/containers/Scoreboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+class Scoreboard extends Component {
+  render() {
+    return (
+      <div className="Scoreboard">
+        Some text here
+      </div>
+    );
+  }
+}
+
+export default Scoreboard;
--- candy/src/index.js
+++ candy/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Scoreboard from './Scoreboard';
+import Scoreboard from './containers/Scoreboard';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Scoreboard />, document.getElementById('root'));
